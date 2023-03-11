d4426f89708261875cc39004a54fe2d740d7ce03
unknown <estiven.carmona@udea.edu.co>
2023-03-11 16:57:52 -0500

    Mini Juego Funcional a falta de estilos y funcionalidades extra

--- Mini_Game/src/main/java/Info/GetSentences.java
+++ Mini_Game/src/main/java/Info/GetSentences.java
@@ -2,17 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package CRUD;
+package Info;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 /**
  *
